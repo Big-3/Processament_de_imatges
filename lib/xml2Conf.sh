0bf4fb3f9cb9f4cbb6a904da442b577600ab0969
Albert Sáez <albertsigmaeng@gmail.com>
2021-12-12 22:10:59 +0100

    refactor and serialize image vectors

--- lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/home/swaglot/miniconda3/envs/Reconeixedor_resistencies/lib"
-XML2_LIBS="-lxml2 -L/home/swaglot/miniconda3/envs/Reconeixedor_resistencies/lib -lz -L/home/swaglot/miniconda3/envs/Reconeixedor_resistencies/lib -llzma -lpthread -L/home/swaglot/miniconda3/envs/Reconeixedor_resistencies/lib -L/home/swaglot/miniconda3/envs/Reconeixedor_resistencies/lib -licui18n -licuuc -licudata -lm "
-XML2_INCLUDEDIR="-I/home/swaglot/miniconda3/envs/Reconeixedor_resistencies/include/libxml2 -I/home/swaglot/miniconda3/envs/Reconeixedor_resistencies/include"
-MODULE_VERSION="xml2-2.9.12"
-
